838e3addfd05f585e18f482ec4b0a074287979a6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-21 21:05:36 +0100

    Update nop_swp.c

--- nop_swp.c
+++ nop_swp.c
@@ -26,18 +26,23 @@ void nop(stack_t **stack, unsigned int line_number)
 void swap_element(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		handleErrors(8, line_number, "swap");
 
-	stack_t *tmp = (*stack)->next;
-	(*stack)->next = tmp->next;
+	while (stack != NULL && *stack != NULL && (*stack)->next != NULL)
+	{
+		tmp = (*stack)->next;
+		(*stack)->next = tmp->next;
 
-	if (tmp->next != NULL)
-		tmp->next->prev = *stack;
+		if (tmp->next != NULL)
+			tmp->next->prev = *stack;
 
-	tmp->next = *stack;
-	(*stack)->prev = tmp;
-	tmp->prev = NULL;
+		tmp->next = *stack;
+		(*stack)->prev = tmp;
+		tmp->prev = NULL;
 
-	*stack = tmp;
+		*stack = tmp;
+		return; /* Exit the loop after one iteration */
+	}
+	/* Error message if there are less than two elements in the stack */
+	handleErrors(8, line_number, "swap");
 }
+
